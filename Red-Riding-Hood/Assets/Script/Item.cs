608632228ad6079e5db9d438a7c37e6689ba9108
Jeonghoon625 <dr.kim1997@gmail.com>
2021-12-13 18:56:39 +0900

    아이템 cs파일 재생성

---
+++ Red-Riding-Hood/Assets/Script/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type {Flower, Hunter};
+    public Type type;
+    public int value;
+}
