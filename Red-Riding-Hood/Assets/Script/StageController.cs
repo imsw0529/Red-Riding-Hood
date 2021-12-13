aa8e60c31f33c9debf6ec1076af37f67ea9bd5c5
Jeonghoon625 <dr.kim1997@gmail.com>
2021-12-13 17:50:42 +0900

    스테이지 컨트롤러 게임 클리어 추가

--- Red-Riding-Hood/Assets/Script/StageController.cs
+++ Red-Riding-Hood/Assets/Script/StageController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageController : MonoBehaviour
 {
@@ -9,12 +10,25 @@ public class StageController : MonoBehaviour
 
     public int MaxFlowerCount => maxFlowerCount;
     public int CurrentFlowerCount => currentFlowerCount;
-
+    
     [SerializeField]
     private Player player;
-    
+
+    [SerializeField]
+    private string nextSceneName;
+
+    [SerializeField]
+    private GameObject panelStageClear;
+    private bool getAllFlowers = false;
+
+
     private void Awake()
     {
+        //시간 속도 1.0
+        Time.timeScale = 1.0f;
+        //클리어 화면 비활성화
+        panelStageClear.SetActive(false);
+
         maxFlowerCount = player.maxFlower;
         currentFlowerCount = 0;
     }
@@ -22,6 +36,26 @@ public class StageController : MonoBehaviour
     private void Update()
     {
         currentFlowerCount = player.flower;
-    }
 
+        //모든 꽃 획득시
+        if (currentFlowerCount == 5)
+        {
+            getAllFlowers = true;
+            
+            //일시정지
+            Time.timeScale = 0.0f;
+
+            //클리어화면 활성화
+            panelStageClear.SetActive(true);
+        }
+
+        if (getAllFlowers == true)
+        {
+            //Enter 입력
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+        }
+    }
 }
